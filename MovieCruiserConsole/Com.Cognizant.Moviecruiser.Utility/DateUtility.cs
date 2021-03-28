957f890adc2e2d774efb6f2a0c18935c901dc565
Poojitha Manam <65291819+Poojitha-Chandra@users.noreply.github.com>
2021-03-29 00:35:11 +0530

    Add files via upload

--- MovieCruiserConsole/Com.Cognizant.Moviecruiser.Utility/DateUtility.cs
+++ MovieCruiserConsole/Com.Cognizant.Moviecruiser.Utility/DateUtility.cs
@@ -1,1 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace Com.Cognizant.Moviecruiser.Utility
+{
+    public class DateUtility
+    {
+        public static DateTime ConvertToDate(string inputDate)
+        {
+            return DateTime.Parse(inputDate);
+        }
+    }
+}
