88457ee4420c8804bcf7423426eed16ea63450b4
Poojitha Manam <65291819+Poojitha-Chandra@users.noreply.github.com>
2021-03-29 00:33:03 +0530

    Add files via upload

---
+++ MovieCruiserConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.Cognizant.Moviecruiser.Dao;
+
+namespace MovieCruiserConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            MovieDaoCollectionTest.TestGetMovieListAdmin();
+            MovieDaoCollectionTest.TestGetMovieListCustomer();
+            MovieDaoCollectionTest.TestModifyMovie();
+            // MenuItemDaoCollectionTest.TestGetMenuItem();
+
+            /*    CartDaoCollectionTest.TestAddCartItem();
+                CartDaoCollectionTest.TestGetAllCartItems();
+                CartDaoCollectionTest.TestRemoveCartItem();*/
+
+            // Test add item to cart
+            FavoritesDaoCollectionTest.TestAddCartItem();
+        }
+    }
+}
